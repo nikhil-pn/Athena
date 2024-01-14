0aa2bb9eef351832622ace9e2c0e9b78f8b7940c
nikhilpn725 <nikhilpn725@gmail.com>
2024-01-14 19:55:27 +0530

    Added the homePage layout and backend folder

---
+++ backend/backend.js
@@ -0,0 +1,1 @@
+//backend code here
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="flex items-center justify-center h-screen font-bold">
+    <h1 className="flex text-[50px] items-center justify-center h-screen font-bold">
       Hello OpenSource
     </h1>
   );
